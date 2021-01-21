a844347475f980ca68a274aa0d0c5d2916719b5b
james tiu <jamestiulawyer@gmail.com>
2021-01-21 11:46:09 -0700

    thursday progress, morning

---
+++ SodaMachine/Wallet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    class Wallet
+    {
+        //Member Variables (Has A)
+        public string walletName;
+        public double totalBalanceCoins;
+        public List<Coin> Coins = new List<Coin>();
+        Coin penny = new Penny();
+        Coin nickel = new Nickel();
+        Coin dime = new Dime();
+        Coin quarter = new Quarter();
+        
+
+        //Constructor (Spawner)
+        public Wallet(string nameOfWallet)
+        {
+            walletName = nameOfWallet;
+            
+            FillRegister();
+        }
+        //Member Methods (Can Do)
+        //Fills wallet with starting money
+        private void FillRegister()
+        {
+          
+        }
+    }
+}
